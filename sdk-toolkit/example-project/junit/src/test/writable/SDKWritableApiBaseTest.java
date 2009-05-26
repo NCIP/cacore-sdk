550b5b066b553814db2f9fcdcff70160c79a77b1
Santosh garmilla <garmillas>
2009-05-26 21:00:13 +0000

    removed test case for base class
    SVN-Revision: 87

--- sdk-toolkit/example-project/junit/src/test/writable/SDKWritableApiBaseTest.java
+++ sdk-toolkit/example-project/junit/src/test/writable/SDKWritableApiBaseTest.java
@@ -64,8 +64,4 @@ public class SDKWritableApiBaseTest extends TestCase{
 	protected void executeQuery(SDKQuery sdkQuery) {
 		serviceDelegator.executeQuery(sdkQuery);
 	}
-	
-	public void testSDKWritableApiBase(){
-		Assert.assertEquals("basetest","basetest");
-	}
 }
