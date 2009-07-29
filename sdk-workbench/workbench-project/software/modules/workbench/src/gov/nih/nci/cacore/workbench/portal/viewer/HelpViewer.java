0026ad099968b583b834ac798a6dcc9de2e6dacf
Daniel Dumitru <ddumitru>
2009-07-29 16:59:35 +0000

    Changes to re-factor "Open Browser" functionality, and to only open the workbench help manual when the user clicks on the 'open' button.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@247 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- sdk-workbench/workbench-project/software/modules/workbench/src/gov/nih/nci/cacore/workbench/portal/viewer/HelpViewer.java
+++ sdk-workbench/workbench-project/software/modules/workbench/src/gov/nih/nci/cacore/workbench/portal/viewer/HelpViewer.java
@@ -3,6 +3,7 @@ package gov.nih.nci.cacore.workbench.portal.viewer;
 import gov.nih.nci.cacore.workbench.common.LookAndFeel;
 import gov.nih.nci.cacore.workbench.common.ResourceManager;
 import gov.nih.nci.cagrid.common.portal.PortalLookAndFeel;
+import gov.nih.nci.cagrid.common.portal.validation.IconFeedbackPanel;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,9 +11,11 @@ import java.beans.PropertyVetoException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 import org.apache.log4j.Logger;
 
@@ -31,13 +34,16 @@ public class HelpViewer extends WorkbenchViewerBaseComponent {
 	
 	private static final String HELP_URL = ResourceManager.getWorkbenchHelpUrl();
 	
-	private static final String[] browsers = { "firefox", "opera", "konqueror", "epiphany",
-		"seamonkey", "galeon", "kazehakase", "mozilla", "netscape" };
+    // Buttons
+    private JButton openButton = null;
+    private JButton closeButton = null;
 
 	/*
 	 * Primary Panel definitions
 	 */
     private JPanel mainPanel = null;
+    private JPanel buttonPanel = null;
+	private JPanel helpPanel = null;
     
 	// Constructor
     public HelpViewer() {
@@ -73,8 +79,40 @@ public class HelpViewer extends WorkbenchViewerBaseComponent {
 //        	openURL(workbenchManualUrl.getFile());
 //        }
         
-        openURL(HELP_URL);
+//        openURL(HELP_URL);
 
+    }
+    
+    /**
+     * This method initializes the Project Settings jPanel
+     */
+    public JPanel getHelpPanel() {
+        if (helpPanel == null) {
+        	
+            JLabel helpLabel = new JLabel();
+            
+			GridBagConstraints gridBagConstraints10 = new GridBagConstraints();
+			//gridBagConstraints10.fill = java.awt.GridBagConstraints.HORIZONTAL;
+			gridBagConstraints10.anchor = java.awt.GridBagConstraints.CENTER;
+			gridBagConstraints10.gridy = 3;
+			gridBagConstraints10.gridx = 0;
+			gridBagConstraints10.insets = new java.awt.Insets(2, 2, 2, 2);
+			gridBagConstraints10.gridwidth = 3;
+			//gridBagConstraints10.weighty = 1.0D;
+			gridBagConstraints10.weightx = 1.0D; 
+            
+            helpLabel.setText("To open the caCORE Workbench user manual in your default browser, click the 'Open' button below, or go to " + HELP_URL + ".");
+
+        	helpPanel = new JPanel();
+        	helpPanel.setLayout(new GridBagLayout());
+        	helpPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Workbench Help",
+                javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
+                javax.swing.border.TitledBorder.DEFAULT_POSITION, null, PortalLookAndFeel.getPanelLabelColor()));
+            
+        	helpPanel.add(helpLabel, gridBagConstraints10);
+
+        }
+        return helpPanel;
     }
     
     /**
@@ -85,59 +123,78 @@ public class HelpViewer extends WorkbenchViewerBaseComponent {
     private JPanel getMainPanel() {
         if (mainPanel == null) {
             
-			GridBagConstraints gridBagConstraints10 = new GridBagConstraints();
-			gridBagConstraints10.anchor = java.awt.GridBagConstraints.CENTER;
-			gridBagConstraints10.gridy = 1;
-			gridBagConstraints10.insets = new java.awt.Insets(2, 2, 2, 2);
-			gridBagConstraints10.gridx = 0;
-			gridBagConstraints10.gridwidth = 3;
-			gridBagConstraints10.weightx = 1.0D;
+            GridBagConstraints gridBagConstraints10 = new GridBagConstraints();
+            gridBagConstraints10.fill = GridBagConstraints.BOTH;
+            gridBagConstraints10.weighty = 1.0;
+            gridBagConstraints10.weightx = 1.0;
+            
+            GridBagConstraints gridBagConstraints20 = new GridBagConstraints();
+            gridBagConstraints20.insets = new java.awt.Insets(5, 5, 5, 5);
+            gridBagConstraints20.gridx = 0;
+            gridBagConstraints20.gridy = 2;
+            gridBagConstraints20.anchor = java.awt.GridBagConstraints.CENTER;
+            gridBagConstraints20.gridheight = 1;
 
             mainPanel = new JPanel();
             mainPanel.setLayout(new GridBagLayout());
-            mainPanel.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Workbench User Manual",
-                javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
-                javax.swing.border.TitledBorder.DEFAULT_POSITION, null, PortalLookAndFeel.getPanelLabelColor()));
-            
-            JLabel helpLabel = new JLabel();
-            helpLabel.setText("If the default browser does not automatically open to the workbench user manual, go to " + HELP_URL + " for more information.");
-            mainPanel.add(helpLabel, gridBagConstraints10);
+            mainPanel.add(getHelpPanel(), gridBagConstraints10);
+            mainPanel.add(getButtonPanel(), gridBagConstraints20);
         }
         return mainPanel;
     }
     
     /**
-     * This method opens the default browser to the Workbench Help URL
-     * Based upon "Bare Bones Browser Launch" - See http://www.centerkey.com/java/browser/
+     * This method initializes jPanel
+     * 
+     * @return javax.swing.JPanel
      */
-	private void openURL(String url) {
-		String osName = System.getProperty("os.name");
-		try {
-			if (osName.startsWith("Mac OS")) {
-				Class<?> fileMgr = Class.forName("com.apple.eio.FileManager");
-				Method openURL = fileMgr.getDeclaredMethod("openURL",
-						new Class[] {String.class});
-				openURL.invoke(null, new Object[] {url});
-			}
-			else if (osName.startsWith("Windows"))
-				Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-			else { //assume Unix or Linux
-				boolean found = false;
-				for (String browser : browsers)
-					if (!found) {
-						found = Runtime.getRuntime().exec(
-								new String[] {"which", browser}).waitFor() == 0;
-						if (found)
-							Runtime.getRuntime().exec(new String[] {browser, url});
-					}
-				if (!found)
-					throw new Exception(Arrays.toString(browsers));
-			}
-		}
-		catch (Exception e) {
-			JOptionPane.showMessageDialog(null,
-					"Error attempting to launch workbench help in web browser:\n" + e.toString());
-		}
-	}
+    private JPanel getButtonPanel() {
+        if (buttonPanel == null) {
+            buttonPanel = new JPanel();
+            buttonPanel.add(getInstallButton(), null);
+            buttonPanel.add(getCloseButton(), null);
+        }
+        return buttonPanel;
+    }
+
+    /**
+     * This method initializes closeButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getCloseButton() {
+        if (closeButton == null) {
+            closeButton = new JButton();
+            closeButton.setIcon(PortalLookAndFeel.getCloseIcon());
+            closeButton.setText("Cancel");
+            closeButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    dispose();
+                }
+            });
+        }
+        return closeButton;
+    }
+    
+
+    /**
+     * This method initializes jButton
+     * 
+     * @return javax.swing.JButton
+     */
+    private JButton getInstallButton() {
+        if (openButton == null) {
+            openButton = new JButton();
+            openButton.setText("Open");
+            openButton.setIcon(LookAndFeel.getGenerateApplicationIcon());
+            openButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                	openURL(HELP_URL);
+                }
+            });
+        }
+
+        return openButton;
+    }
 
 } 
