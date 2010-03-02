4a32813cc258b149ff696af315f7c8ed67e58100
Daniel Dumitru <ddumitru>
2010-03-02 19:06:38 +0000

    Fix for:
    [GF#23293] - Add a Tooltip for Each Field, Not Just for Error Messages
    [GF#23292] - Get rid of Browse for Project Generation Directory popup.
    
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@573 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- sdk-workbench/workbench-project/software/modules/workbench/src/gov/nih/nci/cacore/workbench/portal/panel/DeployTypeSettingsPanel.java
+++ sdk-workbench/workbench-project/software/modules/workbench/src/gov/nih/nci/cacore/workbench/portal/panel/DeployTypeSettingsPanel.java
@@ -2,6 +2,7 @@ package gov.nih.nci.cacore.workbench.portal.panel;
 
 import gov.nih.nci.cacore.workbench.common.LookAndFeel;
 import gov.nih.nci.cacore.workbench.common.OptionsMapManager;
+import gov.nih.nci.cacore.workbench.common.ResourceManager;
 import gov.nih.nci.cacore.workbench.portal.validation.PanelValidator;
 import gov.nih.nci.cacore.workbench.portal.validation.TabbedPanePropertiesValidator;
 import gov.nih.nci.cacore.workbench.portal.viewer.DeployPropertiesViewer;
@@ -10,6 +11,9 @@ import gov.nih.nci.cagrid.common.portal.PortalLookAndFeel;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -18,18 +22,21 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import org.apache.log4j.Logger;
 
 import com.jgoodies.validation.Severity;
 import com.jgoodies.validation.ValidationResult;
 import com.jgoodies.validation.message.SimpleValidationMessage;
+import com.jgoodies.validation.util.ValidationUtils;
 import com.jgoodies.validation.view.ValidationComponentUtils;
 
 public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
 	
-	
-	private static final Logger log = Logger.getLogger(OptionsMapManager.class);
+	private static final Logger log = Logger.getLogger(DeployTypeSettingsPanel.class);
 	
 	private TabbedPanePropertiesValidator mainPanelValidator = null;
 	private DeployPropertiesViewer parentContainer = null;
@@ -39,11 +46,10 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
 	private static final String REMOTE = "remote";
 	
 	// Validation Message Constants
+	private static final String PROJECT_DIR = "Project generation directory";	
 	private static final String DEPLOY_TYPE = "Deployment Type";
 	
 	private static final String REMOTE_DEPLOY_ENV = "Remote Deployment Environment";
-	
-    private JButton loadPropertiesButton = null;
 
 	public DeployTypeSettingsPanel(DeployPropertiesViewer parentContainer,
 			TabbedPanePropertiesValidator mainPanelValidator) {
@@ -57,11 +63,77 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
 	private JPanel deploySettingsReviewPanel = null;
     
 	//Deploy Settings Panel Component Definitions
+	private JTextField projectDirField = null;
     private JComboBox  deployTypeComboBox = null;
-    
     private JComboBox  remoteDeployEnvComboBox = null;
     
-    // Cross-panel properties
+    //Buttons
+    private JButton projectDirButton = null;
+    private JButton loadPropertiesButton = null;    
+    
+    /**
+     * This method initializes jTextField
+     * 
+     * @return javax.swing.JTextField
+     */
+    private JTextField getProjectDirField() {
+        if (projectDirField == null) {
+        	projectDirField = new JTextField();
+        	
+        	//projectDirField.setText(propsMgr.getDeployPropertyValue("PROJECT_DIR"));
+        	projectDirField.setToolTipText("Enter the directory where the project will be generated");
+        	projectDirField.getDocument().addDocumentListener(new DocumentListener() {
+                public void changedUpdate(DocumentEvent e) {
+                	toggleLoadPropertiesButton();
+//                	mainPanelValidator.setDirty(true);
+                    mainPanelValidator.validateInput();
+                }
+
+                public void removeUpdate(DocumentEvent e) {
+                	toggleLoadPropertiesButton();
+//                	mainPanelValidator.setDirty(true);
+                    mainPanelValidator.validateInput();
+                }
+
+
+                public void insertUpdate(DocumentEvent e) {
+                	toggleLoadPropertiesButton();
+//                	mainPanelValidator.setDirty(true);
+                    mainPanelValidator.validateInput();
+                }
+            });
+        	projectDirField.addFocusListener(new FocusChangeHandler());
+        }
+        return projectDirField;
+    }
+    
+    
+    private final class FocusChangeHandler implements FocusListener {
+
+        public void focusGained(FocusEvent e) {
+            update();
+        }
+
+        public void focusLost(FocusEvent e) {
+            update();
+        }
+
+        private void update() {
+        	mainPanelValidator.validateInput();
+        }
+    }    
+    
+    public void setProjectDir(String projectDir){
+    	getProjectDirField().setText(projectDir);
+    }
+    
+    public void setProjectDirValue(String projectDirFieldValue){
+    	this.getProjectDirField().setText(projectDirFieldValue.replace('\\', '/'));
+    }
+    
+    public String getProjectDir(){
+    	return getProjectDirField().getText().replace('\\', '/');
+    }    
         
     /**
      * This method initializes the Deploy Type Combo Box
@@ -71,6 +143,7 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
     private JComboBox getDeployTypeComboBox() {
     	if (deployTypeComboBox == null) {
     		deployTypeComboBox = new JComboBox(); 
+    		deployTypeComboBox.setToolTipText("Specify whether deploying to a Local or Remote environment.");
     		
     		Map<String,String> deployTypeOptionsMap = OptionsMapManager.getDeployTypeOptionsMap();
         	if (deployTypeOptionsMap != null){
@@ -108,6 +181,7 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
     private JComboBox getRemoteDeployEnvComboBox() {
     	if (remoteDeployEnvComboBox == null) {
     		remoteDeployEnvComboBox = new JComboBox(); 
+    		remoteDeployEnvComboBox.setToolTipText("Specify which of the five remote environments (Development, Quality Assurance (QA), Stage, Production, Other) should be targeted.");
     		
     		Map<String,String> remoteDeployEnvOptionsMap = OptionsMapManager.getRemoteDeployEnvOptionsMap();
         	if (remoteDeployEnvOptionsMap != null){
@@ -166,13 +240,46 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
      * 
      * @return javax.swing.JButton
      */
+    private JButton getProjectDirButton() {
+        if (projectDirButton == null) {
+        	projectDirButton = new JButton();
+        	projectDirButton.setText("Browse");
+        	projectDirButton.setIcon(LookAndFeel.getBrowseIcon());
+        	projectDirButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    try {
+                        String previous = getProjectDirField().getText();
+                        String location = ResourceManager.promptDir(previous);
+                        if (location != null && location.length() > 0) {
+                        	getProjectDirField().setText(location);
+                        } else {
+                        	getProjectDirField().setText(previous);
+                        }
+                        mainPanelValidator.validateInput();
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                    
+                }
+            });
+        }
+        return projectDirButton;
+    }    
+    
+    /**
+     * This method initializes jButton
+     * 
+     * @return javax.swing.JButton
+     */
     private JButton getLoadPropertiesButton() {
         if (loadPropertiesButton == null) {
             loadPropertiesButton = new JButton();
             loadPropertiesButton.setText("Load/Create Configuration");
+            loadPropertiesButton.setToolTipText("Click to load the specified configuration properties to the Workbench.");
             loadPropertiesButton.setIcon(LookAndFeel.getGenerateApplicationIcon());
             loadPropertiesButton.addActionListener(new java.awt.event.ActionListener() {
                 public void actionPerformed(java.awt.event.ActionEvent e) {
+                	parentContainer.setProjectDir(getProjectDir());
                 	parentContainer.loadDeployProperties();
                     mainPanelValidator.validateInput();
                 }
@@ -183,6 +290,13 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
     }
     
     public void toggleLoadPropertiesButton() {
+    	
+    	if (ValidationUtils.isBlank(this.getProjectDirField().getText()) ||
+    			!(new File(this.getProjectDirField().getText()).exists()) ) {
+    		this.getLoadPropertiesButton().setEnabled(false);
+    		return;
+    	}
+    	
     	String deployType = getDeployTypeComboBox().getSelectedItem().toString();
     	
     	if (deployType.equalsIgnoreCase(LOCAL)){
@@ -214,43 +328,70 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
 		if (deploySettingsPanel == null) {
 			
 		    //Security Settings Panel Label Definitions
+			JLabel projectDirLabel = null;
 		    JLabel deployTypeLabel = null;
+		    
+            GridBagConstraints gridBagConstraints10 = new GridBagConstraints();
+            gridBagConstraints10.anchor = java.awt.GridBagConstraints.WEST;
+            gridBagConstraints10.gridy = 1;
+            gridBagConstraints10.insets = new java.awt.Insets(2, 2, 2, 2);
+            gridBagConstraints10.gridx = 0;
+            
+            GridBagConstraints gridBagConstraints11 = new GridBagConstraints();
+            gridBagConstraints11.fill = java.awt.GridBagConstraints.HORIZONTAL;
+            gridBagConstraints11.anchor = java.awt.GridBagConstraints.WEST;
+            gridBagConstraints11.gridx = 1;
+            gridBagConstraints11.insets = new java.awt.Insets(2, 2, 2, 2);
+            gridBagConstraints11.gridy = 1;
+//            gridBagConstraints11.weighty = 1.0D;
+            gridBagConstraints11.weightx = 1.0;          
+            
+            GridBagConstraints gridBagConstraints12 = new GridBagConstraints();
+            gridBagConstraints12.anchor = java.awt.GridBagConstraints.WEST;
+            gridBagConstraints12.gridy = 1;
+            gridBagConstraints12.gridx = 2;
+            gridBagConstraints12.insets = new java.awt.Insets(2, 2, 2, 2);
+            gridBagConstraints12.gridwidth = 1;
 	        
-			GridBagConstraints gridBagConstraints10 = new GridBagConstraints();
-			gridBagConstraints10.anchor = java.awt.GridBagConstraints.WEST;
-			gridBagConstraints10.gridy = 1;
-			gridBagConstraints10.insets = new java.awt.Insets(2, 2, 2, 2);
-			gridBagConstraints10.gridx = 0;
-
-			GridBagConstraints gridBagConstraints11 = new GridBagConstraints();
-			gridBagConstraints11.fill = java.awt.GridBagConstraints.HORIZONTAL;
-			gridBagConstraints11.anchor = java.awt.GridBagConstraints.WEST;
-			gridBagConstraints11.gridx = 1;
-			gridBagConstraints11.insets = new java.awt.Insets(2, 2, 2, 2);
-			gridBagConstraints11.gridy = 1;
-			//gridBagConstraints11.weighty = 1.0D;
-			gridBagConstraints11.weightx = 1.0D;  
-			gridBagConstraints11.gridwidth = 2;
-			
 			GridBagConstraints gridBagConstraints20 = new GridBagConstraints();
-			gridBagConstraints20.fill = java.awt.GridBagConstraints.HORIZONTAL;
 			gridBagConstraints20.anchor = java.awt.GridBagConstraints.WEST;
 			gridBagConstraints20.gridy = 2;
-			gridBagConstraints20.gridx = 0;
 			gridBagConstraints20.insets = new java.awt.Insets(2, 2, 2, 2);
-			gridBagConstraints20.gridwidth = 3;
-			//gridBagConstraints20.weighty = 1.0D;
-			gridBagConstraints20.weightx = 1.0D;  
+			gridBagConstraints20.gridx = 0;
+
+			GridBagConstraints gridBagConstraints21 = new GridBagConstraints();
+			gridBagConstraints21.fill = java.awt.GridBagConstraints.HORIZONTAL;
+			gridBagConstraints21.anchor = java.awt.GridBagConstraints.WEST;
+			gridBagConstraints21.gridx = 1;
+			gridBagConstraints21.insets = new java.awt.Insets(2, 2, 2, 2);
+			gridBagConstraints21.gridy = 2;
+			//gridBagConstraints11.weighty = 1.0D;
+			gridBagConstraints21.weightx = 1.0D;  
+			gridBagConstraints21.gridwidth = 2;
 			
 			GridBagConstraints gridBagConstraints30 = new GridBagConstraints();
-			//gridBagConstraints30.fill = java.awt.GridBagConstraints.HORIZONTAL;
-			gridBagConstraints30.anchor = java.awt.GridBagConstraints.CENTER;
+			gridBagConstraints30.fill = java.awt.GridBagConstraints.HORIZONTAL;
+			gridBagConstraints30.anchor = java.awt.GridBagConstraints.WEST;
 			gridBagConstraints30.gridy = 3;
 			gridBagConstraints30.gridx = 0;
 			gridBagConstraints30.insets = new java.awt.Insets(2, 2, 2, 2);
 			gridBagConstraints30.gridwidth = 3;
 			//gridBagConstraints20.weighty = 1.0D;
 			gridBagConstraints30.weightx = 1.0D;  
+			
+			GridBagConstraints gridBagConstraints40 = new GridBagConstraints();
+			//gridBagConstraints30.fill = java.awt.GridBagConstraints.HORIZONTAL;
+			gridBagConstraints40.anchor = java.awt.GridBagConstraints.CENTER;
+			gridBagConstraints40.gridy = 5;
+			gridBagConstraints40.gridx = 0;
+			gridBagConstraints40.insets = new java.awt.Insets(2, 2, 2, 2);
+			gridBagConstraints40.gridwidth = 3;
+			//gridBagConstraints20.weighty = 1.0D;
+			gridBagConstraints40.weightx = 1.0D;
+			
+            projectDirLabel = new JLabel();
+            projectDirLabel.setText("Enter the project directory:");
+            projectDirLabel.setName("Project Directory");
 			
 			deployTypeLabel = new JLabel();
 			deployTypeLabel.setText("Select Deployment Type:");
@@ -261,10 +402,13 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
 					javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION,
 					javax.swing.border.TitledBorder.DEFAULT_POSITION, null, PortalLookAndFeel.getPanelLabelColor()));
 		    
-		    deploySettingsPanel.add(deployTypeLabel, gridBagConstraints10);
-		    deploySettingsPanel.add(getDeployTypeComboBox(), gridBagConstraints11);
-		    deploySettingsPanel.add(getDeployRemoteSettingsSubPanel(), gridBagConstraints20);
-		    deploySettingsPanel.add(getLoadPropertiesButton(), gridBagConstraints30);
+		    deploySettingsPanel.add(projectDirLabel, gridBagConstraints10);
+		    deploySettingsPanel.add(getProjectDirField(), gridBagConstraints11);
+		    deploySettingsPanel.add(getProjectDirButton(), gridBagConstraints12);
+		    deploySettingsPanel.add(deployTypeLabel, gridBagConstraints20);
+		    deploySettingsPanel.add(getDeployTypeComboBox(), gridBagConstraints21);
+		    deploySettingsPanel.add(getDeployRemoteSettingsSubPanel(), gridBagConstraints30);
+		    deploySettingsPanel.add(getLoadPropertiesButton(), gridBagConstraints40);
 			
 		    deploySettingsPanel.validate();
 		}
@@ -392,6 +536,16 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
 
     	ValidationResult result = new ValidationResult();
     	
+    	//Project Directory Setting Validation
+    	if (ValidationUtils.isBlank(this.getProjectDirField().getText())) {
+    		result.add(new SimpleValidationMessage(PROJECT_DIR + " must not be blank.", Severity.ERROR, PROJECT_DIR));
+    	} else {
+    		File file = new File(this.getProjectDirField().getText());
+    		if(file.exists()){
+    			result.add(new SimpleValidationMessage(PROJECT_DIR + " already exists.  The output directory will be overwritten.", Severity.WARNING, PROJECT_DIR));
+    		}
+    	}
+    	
     	//Deploy Type Setting Validation	
        	String deployType = getDeployTypeComboBox().getSelectedItem().toString();
   
@@ -413,6 +567,8 @@ public final class DeployTypeSettingsPanel implements Panel, PanelValidator {
     
     public void initValidation() {
         // Deploy Type Validation
+        ValidationComponentUtils.setMessageKey(getProjectDirField(), PROJECT_DIR);
+        ValidationComponentUtils.setMandatory(getProjectDirField(), true);
         ValidationComponentUtils.setMessageKey(getDeployTypeComboBox(), DEPLOY_TYPE);
         ValidationComponentUtils.setMandatory(getDeployTypeComboBox(), true);
         ValidationComponentUtils.setMessageKey(getRemoteDeployEnvComboBox(), REMOTE_DEPLOY_ENV);
