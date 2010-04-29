018f3288d9de816f6d468291a654ed4e23a5942f
Santosh garmilla <garmillas>
2010-04-29 17:54:06 +0000

    updated XSD test cases with ISO datatypes values
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@772 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- sdk-toolkit/iso-example-project/junit/src/test/xml/mapping/SDKXSDTestBase.java
+++ sdk-toolkit/iso-example-project/junit/src/test/xml/mapping/SDKXSDTestBase.java
@@ -21,18 +21,19 @@ public abstract class SDKXSDTestBase extends TestCase {
 
 	private String namespace = "http://www.w3.org/2001/XMLSchema";
 	private String prefix = "xs";
-	
+
 	protected boolean useGMETags;
 	protected boolean usePermissibleValues;
-	private String namespaceUriPrefix=null;
+	private String namespaceUriPrefix = null;
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		useGMETags=Boolean.parseBoolean(System.getProperty("useGMETags"));
-		usePermissibleValues=Boolean.parseBoolean(System.getProperty("usePermissibleValues"));
+		useGMETags = Boolean.parseBoolean(System.getProperty("useGMETags"));
+		usePermissibleValues = Boolean.parseBoolean(System
+				.getProperty("usePermissibleValues"));
 		log.debug("useGMETags: " + useGMETags);
 		log.debug("usePermissibleValues: " + usePermissibleValues);
-		namespaceUriPrefix=System.getProperty("namespaceUriPrefix");
+		namespaceUriPrefix = System.getProperty("namespaceUriPrefix");
 	}
 
 	protected void tearDown() throws Exception {
@@ -51,54 +52,46 @@ public abstract class SDKXSDTestBase extends TestCase {
 			SAXBuilder builder = new SAXBuilder();
 			Document doc = builder.build(filename);
 			rootElement = doc.getRootElement();
-
 		} catch (Exception ex) {
-			throw new RuntimeException("Error reading XSD file: " + filename,
-					ex);
+			throw new RuntimeException("Error reading XSD file: " + filename,ex);
 		}
-
 		return rootElement;
 	}
-	
+
 	protected Document getDocument(String filename) {
-		if (useGMETags){
-			return getDocument(namespaceUriPrefix,filename);
+		if (useGMETags) {
+			return getDocument(namespaceUriPrefix, filename);
 		}
-		return getDocument("",filename);
+		return getDocument("", filename);
 	}
 
-	protected Document getDocument(String namespacePrefix,String filename) {
+	protected Document getDocument(String namespacePrefix, String filename) {
 		Document doc;
 		try {
-			if (useGMETags){
-				filename=namespacePrefix+filename;
-				filename=filename.replace("://", "_");
-				filename=filename.replace("/", "_");
+			if (useGMETags) {
+				filename = namespacePrefix + filename;
+				filename = filename.replace("://", "_");
+				filename = filename.replace("/", "_");
 			}
-			log.debug("schema filename: "+filename);
+			log.debug("schema filename: " + filename);
 			SAXBuilder builder = new SAXBuilder();
-			ClassPathResource  classPathResource=new ClassPathResource(filename);
+			ClassPathResource classPathResource = new ClassPathResource(
+					filename);
 			doc = builder.build(classPathResource.getInputStream());
 		} catch (Exception ex) {
 			throw new RuntimeException("Error reading XSD file: " + filename,ex);
 		}
-
 		return doc;
 	}
 
 	protected List<Element> queryXSD(Object obj, String xpath)
-	throws JaxenException {
-
+			throws JaxenException {
 		// e.g.: xpath = "/*[local-name()='schema']";
-
 		JDOMXPath path = new JDOMXPath(xpath);
 		path.addNamespace(prefix, namespace);
 		List<Element> elts = path.selectNodes(obj);
-
 		// System.out.println("Elements Found: " + elts.size());
-
 		return elts;
-
 	}
 
 	/**
@@ -109,9 +102,7 @@ public abstract class SDKXSDTestBase extends TestCase {
 	 * @throws Exception
 	 */
 	public void validateCommonSchemaElements() throws Exception {
-
 		Document doc = getDoc();
-
 		assertNotNull(doc);
 		assertNotNull(doc.getRootElement());
 		String xpath = "/xs:schema";
@@ -124,10 +115,9 @@ public abstract class SDKXSDTestBase extends TestCase {
 		xpath = "/xs:schema/xs:complexType";
 		List<Element> complexElts = queryXSD(doc, xpath);
 
-		// Verifies that the number 'element' and 'complexType'  
+		// Verifies that the number 'element' and 'complexType'
 		// Class elements match
 		assertEquals(elts.size(), complexElts.size());
-
 	}
 
 	/**
@@ -137,39 +127,35 @@ public abstract class SDKXSDTestBase extends TestCase {
 	 * 
 	 * @throws Exception
 	 */
-	public void validateClassElements(Class klass) throws Exception{
+	public void validateClassElements(Class klass) throws Exception {
 		validateClassElements(klass, klass.getSimpleName());
 	}
+
 	public void validateClassElements(Class klass, String klassAlias)
-	throws Exception {
-
+			throws Exception {
 		Document doc = getDoc();
-
 		String klassName = klassAlias;
 		String xpath = "/xs:schema/xs:element[@name='" + klassName + "']";
-
 		List<Element> elts = queryXSD(doc, xpath);
 		assertEquals(1, elts.size());
 		Element klassElt = elts.get(0);
-		
-		if (!(null == klassElt.getAttribute("abstract")) && klassElt.getAttributeValue("abstract").equals("true")){
+
+		if (!(null == klassElt.getAttribute("abstract"))
+				&& klassElt.getAttributeValue("abstract").equals("true")) {
 			assertEquals(3, klassElt.getAttributes().size());
-		} else{
+		} else {
 			assertEquals(2, klassElt.getAttributes().size());
 		}
-		
 		assertTrue(klassElt.getAttributeValue("name").equals(klassName));
 		assertTrue(klassElt.getAttributeValue("type").equals(klassName));
-
 		xpath = "/xs:schema/xs:complexType[@name='" + klassName + "']";
-
 		elts = queryXSD(doc, xpath);
 		assertEquals(1, elts.size());
 		Element complexTypeElt = elts.get(0);
 		assertEquals(1, complexTypeElt.getAttributes().size());
 		assertTrue(complexTypeElt.getAttributeValue("name").equals(klassName));
 	}
-	
+
 	/**
 	 * Uses xpath to query the generated XSD Verifies that common elements
 	 * attributes(name, type) are present Verifies that the element 'name'
@@ -177,18 +163,15 @@ public abstract class SDKXSDTestBase extends TestCase {
 	 * 
 	 * @throws Exception
 	 */
-	public void validateClassIsAbstract(Class klass)
-	throws Exception {
-
+	public void validateClassIsAbstract(Class klass) throws Exception {
 		Document doc = getDoc();
-
 		String klassName = klass.getSimpleName();
 		String xpath = "/xs:schema/xs:element[@name='" + klassName + "']";
 
 		List<Element> elts = queryXSD(doc, xpath);
 		assertEquals(1, elts.size());
 		Element klassElt = elts.get(0);
-		
+
 		assertNotNull(klassElt.getAttributeValue("abstract"));
 		assertTrue(klassElt.getAttributeValue("abstract").equals("true"));
 	}
@@ -200,26 +183,25 @@ public abstract class SDKXSDTestBase extends TestCase {
 	 * 
 	 * @throws Exception
 	 */
-	public void validateSubclassElements(Class klass)
-	throws Exception {
-		validateSubclassElements(klass,klass.getSimpleName(),getSuperklassName(klass));
+	public void validateSubclassElements(Class klass) throws Exception {
+		validateSubclassElements(klass, klass.getSimpleName(),
+				getSuperklassName(klass));
 	}
-	
-	public void validateSubclassElements(Class klass, String klassAlias, String superklassName)
-	throws Exception {
+
+	public void validateSubclassElements(Class klass, String klassAlias,
+			String superklassName) throws Exception {
 
 		Document doc = getDoc();
 
 		validateClassElements(klass, klassAlias);
-			
-		String xpath = "/xs:schema/xs:complexType[@name='"
-			+ klassAlias
-			+ "']/xs:complexContent/xs:extension[@base='" + superklassName
-			+ "']";
+
+		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias
+				+ "']/xs:complexContent/xs:extension[@base='" + superklassName
+				+ "']";
 
 		List<Element> elts = queryXSD(doc, xpath);
 		assertEquals(1, elts.size());
-		
+
 		Element klassElt = elts.get(0);
 		assertEquals(1, klassElt.getAttributes().size());
 		assertTrue(klassElt.getAttributeValue("base").equals(superklassName));
@@ -227,170 +209,188 @@ public abstract class SDKXSDTestBase extends TestCase {
 	}
 
 	/**
-	 * Uses xpath to query the generated XSD
-	 * Verifies that Subclass Association elements
-	 * (extension, sequence) and attributes (base, ref) are present
+	 * Uses xpath to query the generated XSD Verifies that Subclass Association
+	 * elements (extension, sequence) and attributes (base, ref) are present
 	 * 
 	 * @throws Exception
 	 */
-	public void validateClassAssociationElements(Class klass, Class associatedKlass, String rolename, String minOccurs, String maxOccurs)
-	throws Exception {
-		validateClassAssociationElements(klass,klass.getSimpleName(), associatedKlass,associatedKlass.getSimpleName(), rolename, minOccurs,maxOccurs);
+	public void validateClassAssociationElements(Class klass,
+			Class associatedKlass, String rolename, String minOccurs,
+			String maxOccurs) throws Exception {
+		validateClassAssociationElements(klass, klass.getSimpleName(),
+				associatedKlass, associatedKlass.getSimpleName(), rolename,
+				minOccurs, maxOccurs);
 	}
-	public void validateClassAssociationElements(Class klass,String klassAlias,Class associatedKlass,String associatedKlassAlias, String rolename, String minOccurs, String maxOccurs)
-	throws Exception {
+
+	public void validateClassAssociationElements(Class klass,
+			String klassAlias, Class associatedKlass,
+			String associatedKlassAlias, String rolename, String minOccurs,
+			String maxOccurs) throws Exception {
 
 		Document doc = getDoc();
 
-		validateClassElements(klass,klassAlias);
+		validateClassElements(klass, klassAlias);
 
-		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias + "']" 
-			+ "/xs:sequence/xs:element[@name='" + rolename + "']"
-			+ "/xs:complexType/xs:sequence/xs:element[@ref='" + associatedKlassAlias + "']";
+		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias + "']"
+				+ "/xs:sequence/xs:element[@name='" + rolename + "']";
 
 		log.debug("* * * xpath: " + xpath);
-		
+
 		List<Element> elts = queryXSD(doc, xpath);
 		assertEquals(1, elts.size());
-		
-		Element klassElt = elts.get(0);
-		assertEquals(3, klassElt.getAttributes().size()); // ref, minOccurs, maxOccurs
-		assertEquals(klassElt.getAttributeValue("ref"),associatedKlassAlias);
-		
-		// TODO :: change to honor minOccurs value passed in
-		// ignore 'minOccurs' value for now, as the Code Generator always sets it to zero
-		assertEquals(klassElt.getAttributeValue("minOccurs"),"0");
-		assertEquals(klassElt.getAttributeValue("maxOccurs"),maxOccurs);
 
-	}	
+		Element klassElt = elts.get(0);
+		//assertEquals(4, klassElt.getAttributes().size()); 
+		assertEquals(klassElt.getAttributeValue("minOccurs"), "0");
+		assertEquals(klassElt.getAttributeValue("maxOccurs"), maxOccurs);
+
+	}
 	
 	/**
-	 * Uses xpath to query the generated XSD
-	 * Verifies that Subclass Association elements
-	 * (extension, sequence) and attributes (base, ref) are present
+	 * Uses xpath to query the generated XSD Verifies that Subclass Association
+	 * elements (extension, sequence) and attributes (base, ref) are present
 	 * 
 	 * @throws Exception
 	 */
-	public void validateSubclassAssociationElements(Class klass, Class associatedKlass, String rolename, String minOccurs, String maxOccurs)
-	throws Exception {
-		validateSubclassAssociationElements(klass,klass.getSimpleName(), associatedKlass,associatedKlass.getSimpleName(),klass.getSuperclass().getSimpleName(),rolename,minOccurs,maxOccurs);
+	public void validateSubclassAssociationElements(Class klass,
+			Class associatedKlass, String rolename, String minOccurs,
+			String maxOccurs) throws Exception {
+		validateSubclassAssociationElements(klass, klass.getSimpleName(),
+				associatedKlass, associatedKlass.getSimpleName(), klass
+						.getSuperclass().getSimpleName(), rolename, minOccurs,
+				maxOccurs);
 	}
-	public void validateSubclassAssociationElements(Class klass,String klassAlias,Class associatedKlass,String associatedKlassAlias,String superklassName,String rolename,String minOccurs,String maxOccurs)
-	throws Exception {
+
+	public void validateSubclassAssociationElements(Class klass,
+			String klassAlias, Class associatedKlass,
+			String associatedKlassAlias, String superklassName,
+			String rolename, String minOccurs, String maxOccurs)
+			throws Exception {
 
 		Document doc = getDoc();
 
-		validateClassElements(klass,klassAlias);
-		
-		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias + "']" 
-			+ "/xs:complexContent/xs:extension[@base='" + superklassName + "']"
-			+ "/xs:sequence/xs:element[@name='" + rolename + "']"
-			+ "/xs:complexType/xs:sequence/xs:element[@ref='" + associatedKlassAlias + "']";
-		
+		validateClassElements(klass, klassAlias);
+
+		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias + "']"
+				+ "/xs:complexContent/xs:extension[@base='" + superklassName
+				+ "']" + "/xs:sequence/xs:element[@name='" + rolename + "']"
+				+ "/xs:complexType/xs:sequence/xs:element[@ref='"
+				+ associatedKlassAlias + "']";
+
 		List<Element> elts = queryXSD(doc, xpath);
 		assertEquals(1, elts.size());
-		
+
 		Element klassElt = elts.get(0);
-		assertEquals(3, klassElt.getAttributes().size()); // ref, minOccurs, maxOccurs
-		assertEquals(klassElt.getAttributeValue("ref"),associatedKlassAlias);
-		
+		assertEquals(3, klassElt.getAttributes().size()); // ref, minOccurs,
+															// maxOccurs
+		assertEquals(klassElt.getAttributeValue("ref"), associatedKlassAlias);
+
 		// TODO :: change eventually to honor minOccurs value passed in
-		// ignore 'minOccurs' value for now, as the Code Generator always sets it to zero
-		assertEquals(klassElt.getAttributeValue("minOccurs"),"0");
-		assertEquals(klassElt.getAttributeValue("maxOccurs"),maxOccurs);
+		// ignore 'minOccurs' value for now, as the Code Generator always sets
+		// it to zero
+		assertEquals(klassElt.getAttributeValue("minOccurs"), "0");
+		assertEquals(klassElt.getAttributeValue("maxOccurs"), maxOccurs);
 
-	}		
-
-	protected void validateAttributeElement(Class klass,String attributeName, String attributeType) throws Exception {
-		validateAttributeElement(klass,klass.getSimpleName(),attributeName, attributeType);
 	}
-	
+
+	protected void validateAttributeElement(Class klass, String attributeName,
+			String attributeType) throws Exception {
+		validateAttributeElement(klass, klass.getSimpleName(), attributeName,
+				attributeType);
+	}
+
 	protected void validateAttributeElement(Class klass, String klassAlias,
 			String attributeName, String attributeType) throws Exception {
 
 		Document doc = getDoc();
-
-		String xpath = "/xs:schema/xs:complexType[@name='"
-			+ klassAlias + "']/xs:attribute[@name='"
-			+ attributeName + "']";
+		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias
+				+ "']/xs:sequence/xs:element[@name='" + attributeName + "']";
 
 		List<Element> attributeElts = queryXSD(doc, xpath);
 		assertNotNull(attributeElts);
 		assertEquals(1, attributeElts.size());
 
 		Element elt = attributeElts.get(0);
-		assertEquals(elt.getAttributeValue("type").toLowerCase(),"xs:"+attributeType.toLowerCase());
+		assertEquals(elt.getAttributeValue("type").toLowerCase(), "uri:"
+				+ attributeType.toLowerCase());
 	}
-	
+
 	protected void validateAttributeWithRestriction(Class klass,
-			String attributeName,String attributeType,String permissibleValue) throws Exception {
+			String attributeName, String attributeType, String permissibleValue)
+			throws Exception {
 
 		Document doc = getDoc();
 
 		String xpath = "/xs:schema/xs:complexType[@name='"
-			+ klass.getSimpleName() + "']/xs:attribute[@name='"
-			+ attributeName + "']/xs:simpleType/xs:restriction";
+				+ klass.getSimpleName() + "']/xs:attribute[@name='"
+				+ attributeName + "']/xs:simpleType/xs:restriction";
 
 		List<Element> restrictionElts = queryXSD(doc, xpath);
 		assertNotNull(restrictionElts);
 		assertEquals(1, restrictionElts.size());
 
 		Element restrictionElt = restrictionElts.get(0);
-		assertEquals(restrictionElt.getAttributeValue("base").toLowerCase(),"xs:"+attributeType.toLowerCase());
-		
+		assertEquals(restrictionElt.getAttributeValue("base").toLowerCase(),
+				"xs:" + attributeType.toLowerCase());
+
 		List<Element> enumerationElts = restrictionElt.getChildren();
 		assertNotNull(enumerationElts);
-		log.debug("enumerationElts.size(): "+enumerationElts.size());
+		log.debug("enumerationElts.size(): " + enumerationElts.size());
 
-		Element enumerationElt = locateChild(enumerationElts,permissibleValue);
-		
+		Element enumerationElt = locateChild(enumerationElts, permissibleValue);
+
 		assertNotNull(enumerationElt);
-		assertEquals(enumerationElt.getAttributeValue("value"),permissibleValue);
+		assertEquals(enumerationElt.getAttributeValue("value"),
+				permissibleValue);
 	}
 
 	protected void validateSubclassAttributeElement(Class klass,
 			String attributeName, String attributeType) throws Exception {
-		validateSubclassAttributeElement(klass, klass.getSimpleName(),getSuperklassName(klass),
-				attributeName, attributeType);
+		validateSubclassAttributeElement(klass, klass.getSimpleName(),
+				getSuperklassName(klass), attributeName, attributeType);
 	}
-	protected void validateSubclassAttributeElement(Class klass, String klassAlias, String superklassName,
-			String attributeName, String attributeType) throws Exception {
+
+	protected void validateSubclassAttributeElement(Class klass,
+			String klassAlias, String superklassName, String attributeName,
+			String attributeType) throws Exception {
 
 		Document doc = getDoc();
-		
-		String xpath = "/xs:schema/xs:complexType[@name='"
-			+ klassAlias
-			+ "']/xs:complexContent/xs:extension[@base='" + superklassName
-			+ "']/xs:attribute[@name='" + attributeName + "']";
 
-//		Element elt = queryXSD(doc, xpath).get(0);
-//		xpath = "xs:attribute[@name='" + attributeName + "']";
+		String xpath = "/xs:schema/xs:complexType[@name='" + klassAlias
+				+ "']/xs:complexContent/xs:extension[@base='" + superklassName
+				+ "']/xs:sequence/xs:element[@name='" + attributeName + "']";
+
+		// Element elt = queryXSD(doc, xpath).get(0);
+		// xpath = "xs:attribute[@name='" + attributeName + "']";
 
 		List<Element> attributeElts = queryXSD(doc, xpath);
 		assertNotNull(attributeElts);
 		assertEquals(1, attributeElts.size());
 
 		Element elt = attributeElts.get(0);
-		assertEquals(elt.getAttributeValue("type").toLowerCase(),"xs:"+attributeType.toLowerCase());
+		assertEquals(elt.getAttributeValue("type").toLowerCase(), "uri:"
+				+ attributeType.toLowerCase());
 	}
-	
-	private String getSuperklassName(Class klass){
 
-		if (klass.getPackage().getName().equals(klass.getSuperclass().getPackage().getName()))
+	private String getSuperklassName(Class klass) {
+
+		if (klass.getPackage().getName().equals(
+				klass.getSuperclass().getPackage().getName()))
 			return klass.getSuperclass().getSimpleName();
 
-		return klass.getSuperclass().getPackage().getName() + ":" + klass.getSuperclass().getSimpleName();
+		return klass.getSuperclass().getPackage().getName() + ":"
+				+ klass.getSuperclass().getSimpleName();
 	}
-	
-	private Element locateChild(List<Element> eltList, String permissibleValue){
-		for (Element elt:eltList){
-			log.debug("* * * "+elt.getAttributeValue("value")+"; permissibleValue: "+permissibleValue);
-			if (elt.getAttributeValue("value").equals(permissibleValue)){
-				log.debug("Child found for permissible value: " +elt.getAttributeValue("value"));
+
+	private Element locateChild(List<Element> eltList, String permissibleValue) {
+		for (Element elt : eltList) {
+			log.debug("* * * " + elt.getAttributeValue("value")
+					+ "; permissibleValue: " + permissibleValue);
+			if (elt.getAttributeValue("value").equals(permissibleValue)) {
+				log.debug("Child found for permissible value: "+ elt.getAttributeValue("value"));
 				return elt;
 			}
 		}
-		
 		return null;
 	}
 }
