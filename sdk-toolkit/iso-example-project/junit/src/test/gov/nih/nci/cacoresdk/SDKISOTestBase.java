a54e612f279f02bb39f2124cc6f6f02e87e5cee8
Prasad Konka <prasad.konka@nih.gov>
2010-05-03 17:51:27 +0000

    Changes for XML data test cases
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@839 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- sdk-toolkit/iso-example-project/junit/src/test/gov/nih/nci/cacoresdk/SDKISOTestBase.java
+++ sdk-toolkit/iso-example-project/junit/src/test/gov/nih/nci/cacoresdk/SDKISOTestBase.java
@@ -1,15 +1,30 @@
 package test.gov.nih.nci.cacoresdk;
 
-import gov.nih.nci.iso21090.NullFlavor;
 import gov.nih.nci.system.applicationservice.ApplicationException;
 import gov.nih.nci.system.applicationservice.ApplicationService;
 import gov.nih.nci.system.client.ApplicationServiceProvider;
+import gov.nih.nci.system.client.util.xml.JAXBMarshaller;
+import gov.nih.nci.system.client.util.xml.JAXBUnmarshaller;
+import gov.nih.nci.system.client.util.xml.Marshaller;
+import gov.nih.nci.system.client.util.xml.Unmarshaller;
+import gov.nih.nci.system.client.util.xml.XMLUtility;
+import gov.nih.nci.system.client.util.xml.XMLUtilityException;
 import gov.nih.nci.system.query.hibernate.HQLCriteria;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
+
 import junit.framework.TestCase;
 
+import org.apache.log4j.Logger;
 import org.hibernate.criterion.DetachedCriteria;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+import test.xml.mapping.SDKXMLDataTestBase;
 
 /**
  * Base class used to support SDK ISO 21090 test cases
@@ -20,13 +35,39 @@ public abstract class SDKISOTestBase extends TestCase {
 
 	private ApplicationService appService;
 	private ApplicationService appServiceFromUrl;
+	private static Logger log = Logger.getLogger(SDKXMLDataTestBase.class);
 	
-	protected String II_ROOT_GLOBAL_CONSTANT_VALUE = "2.16.12.123.456.1";
-	protected NullFlavor NULL_FLAVOR_GLOBAL_CONSTANT_VALUE = NullFlavor.MSK;
+	protected boolean useGMETags;
+	private String namespaceUriPrefix=null;
+	
+	private String uriPrefix = "gme://caCORE.caCORE/3.2/";
+	private String filepathPrefix  = "c:/temp/";
+	private String filepathSuffix  = "_test.xml";
+
+	private Marshaller marshaller;
+	private Unmarshaller unmarshaller;
+	protected XMLUtility myUtil;
 
 	protected void setUp() throws Exception {
 		super.setUp();
 		appService = ApplicationServiceProvider.getApplicationService();
+		boolean validate = true;
+		boolean includeXmlDeclaration = true;
+		String namespacePrefix = "gme://caCORE.caCORE/3.2/";
+		String jaxbContextName = "gov.nih.nci.cacoresdk.domain.other.datatype";
+		marshaller = new JAXBMarshaller(includeXmlDeclaration,jaxbContextName,namespacePrefix);
+		unmarshaller = new JAXBUnmarshaller(validate,jaxbContextName);		
+
+		// JAXB
+		myUtil = new XMLUtility(marshaller, unmarshaller);
+		
+		useGMETags=Boolean.parseBoolean(System.getProperty("useGMETags"));
+		namespaceUriPrefix=System.getProperty("namespaceUriPrefix");
+		String outputDir = System.getProperty("outputDir");
+		if(outputDir!=null){
+			filepathPrefix=outputDir;
+		}		
+		
 	}
 
 
@@ -76,7 +117,138 @@ public abstract class SDKISOTestBase extends TestCase {
 	
 	public static String getTestCaseName()
 	{
-		return "SDK Base Test Case";
+		return "SDK Base ISO Test Case";
 	}
+
+	protected void toXML(String fileName, Object resultObj) throws XMLUtilityException, IOException
+	{
+		File myFile = new File(fileName);						
+		log.info("writing data to file "+myFile.getAbsolutePath());
+		FileWriter myWriter = new FileWriter(myFile);
+		
+		myUtil.toXML(resultObj, myWriter);
+		myWriter.close();
+	}
+	
+	protected Object fromXML(String xmlFileName) throws XMLUtilityException {
+		File myFile = new File(xmlFileName);						
+		Object myObj = (Object) myUtil.fromXML(myFile);
+		return myObj;
+	}
+
+	protected void validateClassElements(org.jdom.Document doc, Object resultObj) throws ClassNotFoundException{
+		validateClassElements(doc, resultObj,Class.forName(getClassName(resultObj)).getSimpleName());
+	}
+	
+	protected void validateClassElements(org.jdom.Document doc, Object resultObj, String gmeClassName) throws ClassNotFoundException{
+		
+		String klassName = getClassName(resultObj);
+		
+		Element klassElt = doc.getRootElement();
+		
+		if (useGMETags){
+			assertEquals(gmeClassName, klassElt.getName());
+		} else{
+			assertEquals(Class.forName(klassName).getSimpleName(), klassElt.getName());
+			assertEquals(klassElt.getNamespaceURI(),uriPrefix + Class.forName(klassName).getPackage().getName());			
+		}
+	}
+
+	protected String getClassName(Object resultObj){
+		String klassName = resultObj.getClass().getName();
+		if (klassName.indexOf('$') > 0) {
+			return klassName.substring(0, klassName.indexOf('$'));
+		}
+			
+		return klassName;
+	}
+	
+	protected String getXMLFileName(Object resultObj, int index)
+	{
+		String className = resultObj.getClass().getSimpleName();
+		if (className.indexOf('$') > 0) {
+			className = className.substring(0, className.indexOf('$'));
+		}
+		
+		String fileName = filepathPrefix + className;
+		if (index != 0){
+			fileName += ("_test"+index+".xml");
+		} else {
+			fileName += filepathSuffix;
+		}
+
+		return fileName;
+	}
+
+	protected org.jdom.Document getDocument(String filename) {
+		org.jdom.Document doc;
+		try {
+			SAXBuilder builder = new SAXBuilder();
+			doc = builder.build(filename);
+		} catch (Exception ex) {
+			throw new RuntimeException("Error reading XML Data file: " + filename,ex);
+		}
+
+		return doc;
+	}
+
+	@SuppressWarnings("unchecked")
+	protected void validateIsoElement(org.jdom.Document doc, String elementName, String elementAttributeName, Object elementAttributeValue)
+	throws Exception {
+		
+		Element klassElt = doc.getRootElement();
+		
+		List<Element> children = klassElt.getChildren();
+		assertNotNull(children);
+
+		assertEquals(1,countChildren(children,elementName));
+		Element element = locateChild(children,elementName);
+		assertNotNull(element);
+		assertEquals(element.getName(),elementName);
+		
+		String compareValue = null;
+		if (elementAttributeValue != null)
+			compareValue = elementAttributeValue.toString();
+		
+		assertEquals(element.getAttributeValue(elementAttributeName),compareValue);
+	}	
+
+	private Element locateChild(List<Element> eltList, String roleName){
+		
+		for (Element elt:eltList){
+			if (elt.getName().equals(roleName)) return elt;
+		}
+		
+		return null;
+	}
+
+	private int countChildren(List<Element> eltList, String roleName){
+		
+		int counter=0;
+		for (Element elt:eltList){
+			if (elt.getName().equals(roleName)) counter++;
+		}
+		
+		return counter;
+	}
+
+	protected String getSchemaFileName(Class klass)
+	{
+		String schemaFileName=klass.getPackage().getName() + ".xsd";	
+		
+		if (useGMETags){
+			schemaFileName=namespaceUriPrefix+schemaFileName;
+			schemaFileName=schemaFileName.replace("://", "_");
+			schemaFileName=schemaFileName.replace("/", "_");
+		}
+		return schemaFileName;
+	}
+
+	protected boolean validateXMLData(Object resultObj, String schemaFilename, String xmlFileName) throws XMLUtilityException{
+		File myFile = new File(xmlFileName);
+		myUtil.fromXML(myFile);
+		return !((JAXBUnmarshaller)unmarshaller).hasValidationExceptions();
+	}
+	
 	
 }
