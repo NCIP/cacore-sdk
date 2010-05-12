b8ae9b1378bcb739b7836cf8196ddf732a4e300b
Prasad Konka <prasad.konka@nih.gov>
2010-05-12 21:02:31 +0000

    DSET transformer changes. This is required to support DSET with NullFlavor set
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@920 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

---
+++ sdk-toolkit/software/modules/system-client/src/gov/nih/nci/system/client/util/xml/JAXBISODsetIiAdapter.java
@@ -0,0 +1,32 @@
+package gov.nih.nci.system.client.util.xml;
+
+import gov.nih.nci.iso21090.*;
+import gov.nih.nci.iso21090.grid.dto.transform.iso.DSETIITransformer;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+import org.iso._21090.ANY;
+import org.iso._21090.DSetII;
+
+public class JAXBISODsetIiAdapter<T1 extends org.iso._21090.ANY, T2 extends gov.nih.nci.iso21090.Any> extends XmlAdapter<T1,T2> {
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public ANY marshal(Any arg0) throws Exception {
+		if(arg0 == null)
+			return null;
+
+		return (org.iso._21090.ANY)DSETIITransformer.INSTANCE.toXml((DSet<Ii>)arg0);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Any unmarshal(org.iso._21090.ANY arg0) throws Exception {
+		if(arg0 == null)
+			return null;
+
+		return (gov.nih.nci.iso21090.Any)DSETIITransformer.INSTANCE.toDto((DSetII)arg0);
+		
+	}
+	
+}
