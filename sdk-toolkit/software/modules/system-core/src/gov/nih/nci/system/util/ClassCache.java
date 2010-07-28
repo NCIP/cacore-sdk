d2a4fc8fb71863cc85edb2f80319188c9dd3f880
Santosh garmilla <garmillas>
2010-07-28 20:57:07 +0000

    fix for DsetTelDataType,DsetAdDataType Persistent Metadata
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@1083 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- sdk-toolkit/software/modules/system-core/src/gov/nih/nci/system/util/ClassCache.java
+++ sdk-toolkit/software/modules/system-core/src/gov/nih/nci/system/util/ClassCache.java
@@ -800,7 +800,7 @@ public class ClassCache implements InitializingBean{
 					discriminatorMap.put(pklass.getClassName(), identifier);
 				}
 			}
-/*			if (dao instanceof ORMDAOImpl) {
+			if (dao instanceof ORMDAOImpl) {
 				Configuration cfg = ((ORMDAOImpl) dao).getConfig();
 				for (String className : allClassNames) {
 					Map<String, Map<String, List<Object>>> tempSearchFieldForObject = getMapOfSearchFields(
@@ -808,7 +808,7 @@ public class ClassCache implements InitializingBean{
 					if (tempSearchFieldForObject != null)
 						searchableFieldsMap.putAll(tempSearchFieldForObject);
 				}		
-			}*/
+			}
 		}
 
 		allPackageNamesCache = new ArrayList<String>(tmpPackageNames);
