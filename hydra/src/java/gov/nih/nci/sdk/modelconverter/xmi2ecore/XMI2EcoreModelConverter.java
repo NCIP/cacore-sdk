55524b7526093413843b60b1ad2f848528b3b21a
John Chen <chenj>
2010-08-27 23:16:41 +0000

    adding tag to ecore
    SVN-Revision: 1166

--- hydra/src/java/gov/nih/nci/sdk/modelconverter/xmi2ecore/XMI2EcoreModelConverter.java
+++ hydra/src/java/gov/nih/nci/sdk/modelconverter/xmi2ecore/XMI2EcoreModelConverter.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.sdk.modelconverter.xmi2ecore;
 
+import gov.nih.nci.sdk.modelconverter.util.SDKTag;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,8 +22,14 @@ import org.apache.commons.lang.StringUtils;
 import org.eclipse.emf.codegen.util.CodeGenUtil;
 import org.eclipse.emf.common.util.BasicDiagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EModelElement;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.impl.EClassImpl;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.importer.ModelImporter;
@@ -136,13 +144,15 @@ public class XMI2EcoreModelConverter extends UMLImporter {
 			}
 		}.convert(packages, options, diagnostics, context);
 
-		preparedFile.delete();
-
 		EPackage epkg = null;
 		Iterator<EPackage> it = ePackages.iterator();
 		if (it.hasNext()) {
 			epkg = it.next();
 		}
+		
+		fetchTags(preparedFile, epkg);
+		
+		preparedFile.delete();
 
 		return epkg;
 	}
@@ -229,6 +239,252 @@ public class XMI2EcoreModelConverter extends UMLImporter {
 		out.close();
 
 		return new File(tmpFileName);
+	}
+	
+	void fetchTags(File file, EPackage epkg) throws IOException {
+		if (file == null || epkg == null) return;
+		
+		if (!file.exists())
+			throw new FileNotFoundException("File " + file
+					+ " does not exist.");
+		
+		List<String> lines = new ArrayList<String>();
+		List<TagLine> tagLines = new ArrayList<TagLine>();
+		BufferedReader in = new BufferedReader(new FileReader(file));
+		String line = null;
+		while ((line = in.readLine()) != null) {
+			lines.add(line);
+			
+			if (line.indexOf("<thecustomprofile:") != -1 &&
+					line.indexOf(" base_") != -1) {
+				TagLine tagLine = new TagLine(line);
+				if (tagLine.isValidTag()) {
+					tagLines.add(tagLine);
+				}
+			}
+		}
+		in.close();
+		
+		if (lines.size() == 0 || tagLines.size() == 0) return;
+		
+		fillInSourceForTags(lines, tagLines);
+		linkTagsWithModel(epkg, tagLines);
+	}
+	
+	private void fillInSourceForTags(List<String> lines, List<TagLine> tagLines) {
+		Iterator<TagLine> it = tagLines.iterator();
+		while(it.hasNext()) {
+			TagLine tag = it.next();
+			fillInSourceForTag(lines, tag);
+		}
+	}
+	
+	private void fillInSourceForTag(List<String> lines, TagLine tag) {
+		String searchToken = "xmi:id=\"" + tag.baseId + "\"";
+		Iterator<String> it = lines.iterator();
+		int count = 0;
+		while(it.hasNext()) {
+			String line = it.next();
+			if (line.indexOf(searchToken) != -1) {
+				String source = getValueForToken(line, "name");
+				tag.source = source;
+				if (tag.isClassTag()) {
+					tag.containerClass = source;
+				}
+				else {
+					detectClassName(lines, count, tag);
+				}
+			}
+			count++;
+		}
+	}
+	
+	static String detectClassName(List<String> lines, int index, TagLine tag) {
+		String s = null;
+		for (int i = index; i > 0; i--) {
+			String line = lines.get(i).trim();
+			if (line.startsWith("<packagedElement xmi:type=\"uml:Class\"")) {
+				s = getValueForToken(line, "name");
+				tag.containerClass = s;
+				break;
+			}
+		}
+		return s;
+	}
+	
+	private void linkTagsWithModel(EPackage rootPkg, List<TagLine> tagLines) {
+		Iterator<TagLine> it = tagLines.iterator();
+		while(it.hasNext()) {
+			TagLine tag = it.next();
+			linkTagWithModel(rootPkg, tag);
+		}
+	}
+	
+	private void linkTagWithModel(EPackage rootPkg, TagLine tag) {
+		if (tag.source == null || tag.containerClass == null) return;
+		
+		System.out.println("creating link for tag: " + tag);
+		EModelElement modelElement = null;
+		if (tag.isAttributeTag()) {
+			modelElement = getModelElement(rootPkg, tag.containerClass, tag.source);
+		}
+		else if (tag.isClassTag()) {
+			modelElement = getModelElement(rootPkg, tag.containerClass);
+		}
+		
+		if (modelElement != null) {
+			modelElement.getEAnnotations().add(tag.toSDKTag());
+			System.out.println("xxxxxxxxxxxxxxx added tag to model element named " + modelElement);
+		}
+	}
+	
+	static String getValueForToken(String line, String token) {
+		if (line.endsWith("/>")) {
+			line = line.substring(0, line.length() -2);
+		}
+		else if (line.endsWith(">")) {
+			line = line.substring(0, line.length() -2);
+		}
+		int nameIndex = line.indexOf(token + '=');
+		if (nameIndex == -1) return null;
+		
+		int startIndex = nameIndex + token.length() + 1;
+		int endIndex = line.indexOf(' ', startIndex + 1);
+		String s = (endIndex != -1)?line.substring(startIndex, endIndex):line.substring(startIndex);
+		s = StringUtils.remove(s, '"');
+		return s;
+	}
+	
+	public static EModelElement getModelElement(EPackage pkg, String eClassName) {
+		System.out.println("getModelElement eClassName: " + eClassName);
+		if (pkg == null || eClassName == null) return null;
+		
+		EClass eClass = null;
+		Iterator<EObject> pkgIter = pkg.eContents().iterator();
+		EObject eo = null;
+		while (pkgIter.hasNext()) {
+			eo = pkgIter.next();
+			
+			if (eo instanceof EClassImpl) {
+				EClass tmp = (EClassImpl) eo;
+				if (eClassName.equals(tmp.getName())) {
+					System.out.println("xxxxxFound business entity " + tmp.getName());
+					eClass = tmp;
+					break;
+				}
+			} else if (eo instanceof EPackage) {
+				return getModelElement((EPackage) eo, eClassName);
+			}
+		}
+		
+		System.out.println("xxxxxxxFound business entity " + eClass);
+		
+		return eClass;
+	}
+	
+	public static EModelElement getModelElement(EPackage pkg, String eClassName, String attributeName) {
+		System.out.println("getModelElement eClassName: " + eClassName);
+		System.out.println("getModelElement attributeName: " + attributeName);
+		if (pkg == null || eClassName == null) return null;
+		
+		EClass eClass = null;
+		Iterator<EObject> pkgIter = pkg.eContents().iterator();
+		EObject eo = null;
+		while (pkgIter.hasNext()) {
+			eo = pkgIter.next();
+			
+			if (eo instanceof EClassImpl) {
+				EClass tmp = (EClassImpl) eo;
+				if (eClassName.equals(tmp.getName())) {
+					System.out.println("Parsing attributes for business entity " + tmp.getName());
+					if (attributeName != null) {
+						EList<EStructuralFeature> attributes = tmp.getEAllStructuralFeatures();
+						Iterator<EStructuralFeature> itAttr = attributes.iterator();
+						while (itAttr.hasNext()) {
+							EStructuralFeature attr = itAttr.next();
+							System.out.println("attr: " + attr.getName());
+							if (attributeName.equals(attr.getName())) {
+								eClass = tmp;
+								break;
+							}
+						}
+					}
+				}
+			} else if (eo instanceof EPackage) {
+				getModelElement((EPackage) eo, eClassName, attributeName);
+			}
+		}
+		
+		return eClass;
+	}
+	
+	class TagLine {
+		String baseId;
+		String baseLevel;
+		String name;
+		String value;
+		String source;
+		String containerClass;
+		
+		TagLine(String line) {
+			init(line);
+		}
+		
+		boolean isValidTag() {
+			return value != null;
+		}
+		
+		boolean isAttributeTag() {
+			return "Attribute".equals(baseLevel);
+		}
+		
+		boolean isClassTag() {
+			return "Class".equals(baseLevel);
+		}
+		
+		boolean isOperationTag() {
+			return "Operation".equals(baseLevel);
+		}
+		
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("name=").append(name).append(",");
+			sb.append("value=").append(value).append(",");
+			sb.append("baseLevel=").append(baseLevel).append(",");
+			sb.append("baseId=").append(baseId).append(",");
+			sb.append("source=").append(source).append(",");
+			sb.append("containerClass=").append(containerClass);
+			
+			return sb.toString();
+		}
+		
+		SDKTag toSDKTag() {
+			return new SDKTag(name, value);
+		}
+		
+		private void init(String line) {
+			if (line == null) return;
+			try {
+				//parse the line
+				line = line.trim();
+				name = line.substring("<thecustomprofile:".length(), line.indexOf(' '));
+				name = StringUtils.remove(name, '"');
+				
+				int valueStartIndex = line.indexOf(name + '=');
+				if (valueStartIndex != -1) {
+					value = line.substring(line.lastIndexOf('=') + 2, line.lastIndexOf('"'));
+					
+					String ln = line.substring(line.indexOf("base_") + 5, valueStartIndex -1);
+					ln = StringUtils.remove(ln, '"');
+					baseLevel = ln.substring(0, ln.indexOf('='));
+					
+					baseId = ln.substring(ln.indexOf('=') + 1);
+				}
+			}
+			catch (Exception ex) {
+				throw new IllegalArgumentException("Failed to parse line \"" + line + "\".");
+			}
+		}
 	}
 
 	private static final String searchStringTemplateSignature = "uml:TemplateSignature";
