4a4183c919cae037a08fb01ad95e23817fa7e824
George Bediako <georgeb>
2010-08-27 17:37:25 +0000

    Changed file representation from absolute path as string to a java.io.File object
    SVN-Revision: 1154

--- hydra/src/java/gov/nih/nci/sdk/core/GeneratorConductor.java
+++ hydra/src/java/gov/nih/nci/sdk/core/GeneratorConductor.java
@@ -5,6 +5,5 @@ import org.eclipse.emf.ecore.EPackage;
 public interface GeneratorConductor
 {
 	public void compile(GeneratorContext _generatorContext);
-	public void conduct(GeneratorContext _generatorContext);
-	public java.util.List<Message> level0Validate(GeneratorContext _generatorContext);
+	public void level0Validate(GeneratorContext _generatorContext);
 }
