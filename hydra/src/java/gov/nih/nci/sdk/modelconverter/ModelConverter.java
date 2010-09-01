e2c7af3d1ef7a7df50cf9c6541604c346236480b
John Chen <chenj>
2010-09-01 15:33:22 +0000

    Added ModelConverter
    SVN-Revision: 1198

---
+++ hydra/src/java/gov/nih/nci/sdk/modelconverter/ModelConverter.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.sdk.modelconverter;
+
+import org.eclipse.emf.ecore.EPackage;
+
+public interface ModelConverter {
+
+	/**
+	 * Converts the file content to Ecore model.
+	 * 
+	 * @param filePath
+	 *            full file path to the file.
+	 * @return a root instance of EPackage or <tt>null</tt> if there is none.
+	 * @throws Exception
+	 */
+	public EPackage convert(String filePath) throws Exception;
+}
