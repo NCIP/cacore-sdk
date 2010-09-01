b02900199a427abf1d4efb31d142fe5a90392930
John Chen <chenj>
2010-09-01 17:42:07 +0000

    Fixed code to handle resource instead of hard-coded file path.
    SVN-Revision: 1201

--- hydra/src/test/gov/nih/nci/sdk/modelconverter/xmi2ecore/XMI2EcoreModelConverterTestHelper.java
+++ hydra/src/test/gov/nih/nci/sdk/modelconverter/xmi2ecore/XMI2EcoreModelConverterTestHelper.java
@@ -1,16 +1,43 @@
 package gov.nih.nci.sdk.modelconverter.xmi2ecore;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import gov.nih.nci.sdk.modelconverter.util.ModelConverterUtil;
 
 import org.eclipse.emf.ecore.EPackage;
 
 public class XMI2EcoreModelConverterTestHelper {
-	public static final String TEST_MODEL_XMI_FILE = "./test/model/sdkexample.xmi";
+	public static final String TEST_MODEL_XMI_FILE = "/model/sdkexample.xmi";
 	
 	/**
 	 * Reads in the XMI file as Ecore model package.
 	 */
 	public static EPackage readEPackageFromXMIFile() {
-		return ModelConverterUtil.getEPackageFromXMIFile(TEST_MODEL_XMI_FILE);
+		File xmiFile = createTempFileForTesting(TEST_MODEL_XMI_FILE);
+		return ModelConverterUtil.getEPackageFromXMIFile(xmiFile.getAbsolutePath());
+	}
+	
+	private static File createTempFileForTesting(String xmiResourcePath) {
+		String tmpDir = System.getProperty("java.io.tmpdir");
+		File outFile = new File(tmpDir + File.separatorChar + "_sdkexample.xmi");
+		InputStream in = "".getClass().getResourceAsStream(xmiResourcePath);
+		try {
+			OutputStream out = new FileOutputStream(outFile);
+			byte buf[] = new byte[4096];
+			int len;
+			while((len=in.read(buf))>0) {
+				out.write(buf,0,len);
+			}
+			out.close();
+			in.close();
+		}
+		catch (IOException ex) {
+			throw new IllegalArgumentException("Failed to read file "  + xmiResourcePath + ": " + ex.getMessage());
+		}
+		return outFile;
 	}
 }
