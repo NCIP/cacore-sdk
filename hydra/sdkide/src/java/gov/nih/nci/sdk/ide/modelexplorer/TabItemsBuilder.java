97144bf576543228d76afa5877701adbf16995f7
John Chen <chenj>
2010-09-09 15:46:03 +0000

    Removed unused classes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@1238 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- hydra/sdkide/src/java/gov/nih/nci/sdk/ide/modelexplorer/TabItemsBuilder.java
+++
@@ -1,7 +0,0 @@
-package gov.nih.nci.sdk.ide.modelexplorer;
-
-import org.eclipse.swt.widgets.TabFolder;
-
-public class TabItemsBuilder {
-	//public CategoryTabItem[] build(TabFolder parent, int style, Object data, String title)
-}
