976084ddfa15db4c894c85bc1a39ff696594fab8
John Chen <chenj>
2010-09-09 23:06:53 +0000

    Many updates (better tree display, event passing, etc.)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@1244 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- hydra/src/java/gov/nih/nci/sdk/ide/modelexplorer/ModelSelectionEvent.java
+++ hydra/src/java/gov/nih/nci/sdk/ide/modelexplorer/ModelSelectionEvent.java
@@ -6,15 +6,21 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Event;
 
 public class ModelSelectionEvent extends Event {
+	private String packageName;
 	private String modelName;
 	private String category;
 	private Date timestamp;
 	
-	public ModelSelectionEvent(String modelName, String category) {
+	public ModelSelectionEvent(String packageName, String modelName, String category) {
+		this.packageName = packageName;
 		this.modelName = modelName;
 		this.category = category;
 		this.timestamp = new Date();
 		super.type = SWT.Selection;
+	}
+
+	public String getPackageName() {
+		return packageName;
 	}
 
 	public String getModelName() {
@@ -29,14 +35,20 @@ public class ModelSelectionEvent extends Event {
 		return timestamp;
 	}
 	
+	public String getFullModelName() {
+		return (packageName == null || "".equals(packageName)) ? modelName
+				: (packageName + "." + modelName);
+	}
+	
 	public String getEventName() {
-		return modelName + " - " + category;
+		return getFullModelName() + " - " + category;
 	}
 	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("timestamp=").append(timestamp).append(", ");
-		sb.append("className=").append(modelName).append(", ");
+		sb.append("packageName=").append(packageName).append(", ");
+		sb.append("modelName=").append(modelName).append(", ");
 		sb.append("category=").append(category);
 		return sb.toString();
 	}
