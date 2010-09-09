8e62b6a9b82a571514d45d7e0f5fe080e0ee9abf
John Chen <chenj>
2010-09-09 23:06:53 +0000

    Many updates (better tree display, event passing, etc.)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@1244 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- hydra/src/java/gov/nih/nci/sdk/ide/modelexplorer/SDKModelExplorer.java
+++ hydra/src/java/gov/nih/nci/sdk/ide/modelexplorer/SDKModelExplorer.java
@@ -1,8 +1,13 @@
 package gov.nih.nci.sdk.ide.modelexplorer;
 
 import gov.nih.nci.sdk.ide.core.GroupPanel;
+import gov.nih.nci.sdk.ide.core.ModelPackageVO;
 import gov.nih.nci.sdk.ide.core.SDKScreen;
+import gov.nih.nci.sdk.ide.core.UIHelper;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -10,11 +15,15 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
 
 public class SDKModelExplorer extends SDKScreen {
-	private Object data;
+	private static EPackage epackage;
 	
 	public SDKModelExplorer(Shell parent, String title, Object data) {
 		super(parent, title);
-		this.data = data;
+		epackage = (EPackage)data;
+	}
+	
+	public static EPackage getEPackage() {
+		return epackage;
 	}
 	
 	public void createScreen(Composite composite) {
@@ -26,20 +35,36 @@ public class SDKModelExplorer extends SDKScreen {
 		layout.marginWidth = 10;
 		layout.numColumns = 2;
 		composite.setLayout(layout);
-
-		GroupPanel masterPanel = new ModelMasterGroupPanel(composite, SWT.NONE, data, Constants.MODEL_MASTER_PANEL_TITLE);
+		
+		List<ModelPackageVO> dataList = UIHelper.getModelPackages(epackage);
+		GroupPanel masterPanel = new ModelMasterGroupPanel(composite, SWT.NONE, dataList, Constants.MODEL_MASTER_PANEL_TITLE);
 		masterPanel.paint();
 		
-		String modelName = getFirstModelName();
+		ModelPackageVO firstMP = getFirstModel(dataList);
+		if (firstMP == null) return;
+		String packageName = firstMP.getPackageName();
+		String modelName = firstMP.getModels().get(0);
 		String categoryName = Constants.DEFAULT_CATEGORY;
-		Event defaultEvent = new ModelSelectionEvent(modelName, categoryName);
+		Event defaultEvent = new ModelSelectionEvent(packageName, modelName, categoryName);
+		
 		GroupPanel detailsPanel = new ModelDetailsGroupPanel(composite, SWT.NONE, defaultEvent, Constants.MODEL_DETAILS_PANEL_TITLE);
 		detailsPanel.paint();
 		
 		masterPanel.addListener(SWT.Selection, detailsPanel);
 	}
 	
-	private String getFirstModelName() {
-		return "Person";//TODO: hard-coded for now.
+	private ModelPackageVO getFirstModel(List<ModelPackageVO> dataList) {
+		if (dataList == null || dataList.size() == 0) return null;
+		
+		ModelPackageVO firstModel = null;
+		for (ModelPackageVO mpVO : dataList) {
+			List<String> models = mpVO.getModels();
+			if (models.size() > 0) {
+				firstModel = mpVO;
+				break;
+			}
+		}
+		
+		return firstModel;
 	}
 }
