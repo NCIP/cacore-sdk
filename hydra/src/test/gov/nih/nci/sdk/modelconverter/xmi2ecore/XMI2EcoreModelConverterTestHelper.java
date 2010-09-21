138d317470e080641d77dac3876f2c139f1f1c8b
John Chen <chenj>
2010-09-21 15:54:16 +0000

    Updated exception
    SVN-Revision: 1281

--- hydra/src/test/gov/nih/nci/sdk/modelconverter/xmi2ecore/XMI2EcoreModelConverterTestHelper.java
+++ hydra/src/test/gov/nih/nci/sdk/modelconverter/xmi2ecore/XMI2EcoreModelConverterTestHelper.java
@@ -12,40 +12,39 @@ import org.eclipse.emf.ecore.EPackage;
 
 public class XMI2EcoreModelConverterTestHelper {
 	public static final String TEST_MODEL_XMI_FILE = "/model/sdkexample.xmi";
-	
+
 	/**
 	 * Reads in the XMI file as Ecore model package.
 	 */
 	public static EPackage readEPackageFromXMIFile() {
 		File xmiFile = createTempFileForTesting(TEST_MODEL_XMI_FILE);
-		return ModelConverterUtil.getEPackageFromXMIFile(xmiFile.getAbsolutePath());
+		return ModelConverterUtil.getEPackageFromXMIFile(xmiFile
+				.getAbsolutePath());
 	}
-	
+
 	private static File createTempFileForTesting(String xmiResourcePath) {
 		String tmpDir = System.getProperty("java.io.tmpdir");
 		File outFile = new File(tmpDir + File.separatorChar + "_sdkexample.xmi");
-		InputStream in = XMI2EcoreModelConverterTestHelper.getResourceAsStream(xmiResourcePath);
-		try
-		{
+		InputStream in = XMI2EcoreModelConverterTestHelper
+				.getResourceAsStream(xmiResourcePath);
+		try {
 			OutputStream out = new FileOutputStream(outFile);
 			byte buf[] = new byte[4096];
 			int len;
-			while((len=in.read(buf))>0) {
-				out.write(buf,0,len);
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
 			}
 			out.close();
 			in.close();
+		} catch (Exception ex) {
+			throw new IllegalArgumentException("Failed to read file "
+					+ xmiResourcePath + ": " + ex.getMessage());
 		}
-		catch (IOException ex)
-		{
-			throw new IllegalArgumentException("Failed to read file "  + xmiResourcePath + ": " + ex.getMessage());
-		}
-		
+
 		return outFile;
 	}
 
-	private static InputStream getResourceAsStream(String _resourcePath)
-	{
+	private static InputStream getResourceAsStream(String _resourcePath) {
 		XMI2EcoreModelConverterTestHelper helper = new XMI2EcoreModelConverterTestHelper();
 		return helper.getClass().getResourceAsStream(_resourcePath);
 	}
