1f1fb90590a393ba235625d8204be87a714afeea
George Bediako <georgeb>
2010-09-23 12:11:41 +0000

    first cut on new properties tab
    SVN-Revision: 1290

--- hydra/src/java/gov/nih/nci/sdk/ide/modelexplorer/meaning/MeaningPropertiesTabItem.java
+++ hydra/src/java/gov/nih/nci/sdk/ide/modelexplorer/meaning/MeaningPropertiesTabItem.java
@@ -1,26 +1,47 @@
 package gov.nih.nci.sdk.ide.modelexplorer.meaning;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
+import org.eclipse.swt.layout.GridLayout;
 import gov.nih.nci.sdk.ide.core.CategoryTabItem;
 import gov.nih.nci.sdk.ide.modelexplorer.Constants;
 
 public class MeaningPropertiesTabItem extends CategoryTabItem {
 
-	public MeaningPropertiesTabItem(TabFolder parent, int style, Object data) {
+	public MeaningPropertiesTabItem(TabFolder parent, int style, Object data)
+	{
 		super(parent, style, data, Constants.TAB_Properties);
 	}
 
 	@Override
-	public void paint() {
+	public void paint()
+	{
 		Composite composite = super.getUIComposite();
 		composite.setLayout(super.getLayout());
 		
-		Button button = new Button(composite, SWT.PUSH);
-		button.setText("Replace me for " + super.getTitle());
+		new Label(composite, SWT.NONE).setText("Domain Name");
+		Text domainNameText = new Text(composite, SWT.BORDER | SWT.READ_ONLY);
+		domainNameText.setText("Bediako");
+		domainNameText.setLayoutData(super.getGridData());
+
+		new Label(composite, SWT.NONE).setText("Description");
+		Text domainDescText = new Text(composite, SWT.BORDER | SWT.READ_ONLY | SWT.MULTI);
+		domainDescText.setText("Bediako Description");
+		domainDescText.setLayoutData(super.getGridData());
+
+		List propertiesList = new List(composite, SWT.NONE);
+		propertiesList.add("Bediako");
+		propertiesList.add("George");
+		
+		Composite conceptsArea = new Composite(composite, SWT.NONE);
+		GridLayout cgd = (GridLayout)super.getLayout();
+		cgd.numColumns = 1;
+		conceptsArea.setLayout(cgd);
 		
 		composite.setSize(composite.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 	}
