de2b8dc5e2f7d4adce6a2355c2ff287a2e84a77e
Daniel Dumitru <ddumitru>
2011-01-22 00:26:17 +0000

    Allow ';' as character input.  Characters '[' and ']' are still considered invalid characters, and thus cannot be entered at the UI level, even though they currently exist in our sample DB scripts and tables
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/trunk@1487 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

--- sdk-toolkit/software/workspace/web/sdk-jquery.validate.js
+++ sdk-toolkit/software/workspace/web/sdk-jquery.validate.js
@@ -1,6 +1,6 @@
 	var intRegex = /^-?\d+$/i;
 	var positiveIntRegex = /^\d+$/i;
-	var charRegex = /^\w$/i;
+	var charRegex = /^(\w|\;)$/i;
 	var dateRegex = /^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|20)\d\d$/i;	
 	var mailtoUriRegex = /^mailto:((([a-z]|\d|[!#\$%&amp;'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&amp;'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))(\?.*)*$/i;
 	var telUriRegex = /^(mailto|tel|x-text-tel|x-text-fax):(\/\/)*(\+\d)*\s*(\(\d{3}\)\s*|\d{3}\s*|\d{3}-\s*)*\d{3}(-{0,1}|\s{0,1})\d{2}(-{0,1}|\s{0,1})\d{2}$/i;
