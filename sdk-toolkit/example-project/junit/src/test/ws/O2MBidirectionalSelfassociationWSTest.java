6ee3d5c6a21af52f73ef8f5f8e8e23c1a966d4d4
Daniel Dumitru <ddumitru>
2011-09-06 21:22:55 +0000

    Merged v4.4.1 trunk changes...
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/branches/CACORESDK_45_BRANCH@1634 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

---
+++ sdk-toolkit/example-project/junit/src/test/ws/O2MBidirectionalSelfassociationWSTest.java
@@ -0,0 +1,227 @@
+package test.ws;
+
+import gov.nih.nci.cacoresdk.domain.onetomany.bidirectional.selfassociation.MemberO2MBS;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class O2MBidirectionalSelfassociationWSTest extends SDKWSTestBase
+{
+	public static String getTestCaseName()
+	{
+		return "One to Many Bidirectional Self association WS Test Case";
+	}
+	
+	protected Collection<Class> getClasses() throws Exception
+	{	
+		Collection<Class> mappedKlasses = new ArrayList<Class>();
+		
+		mappedKlasses.add(MemberO2MBS.class);
+				
+		return mappedKlasses;
+	}
+	
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * 
+	 * @throws Exception
+	 */
+	public void testEntireObjectNestedSearch1() throws Exception
+	{
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(4,results.length);
+		
+		for (Object obj : results){
+			MemberO2MBS result = (MemberO2MBS)obj;
+			assertNotNull(result);
+			assertNotNull(result.getId());
+			assertNotNull(result.getName());	
+		}			
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * 
+	 * @throws Exception
+	 */
+	public void testEntireObjectNestedSearch2() throws Exception
+	{
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(4,results.length);
+		
+		for (Object obj : results){
+			MemberO2MBS result = (MemberO2MBS)obj;
+			assertNotNull(result);
+			assertNotNull(result.getId());
+			assertNotNull(result.getName());	
+		}		
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * erifies that the associated object is null
+	 * 
+	 * @throws Exception
+	 */
+	public void testZeroAssociatedObjectsNestedSearch1() throws Exception
+	{
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+		criteria.setId(new Integer(3));
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(1,results.length);
+		
+		MemberO2MBS result = (MemberO2MBS)results[0];
+		assertNotNull(result);
+		assertNotNull(result.getId());
+		assertNotNull(result.getName());	
+		
+		Object[] mentorCollection = getAssociationResults(result, "mentorCollection", 0);
+		assertEquals(0,mentorCollection.length);
+	
+	}
+
+		
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * Verifies that the associated object has required Id
+	 * 
+	 * @throws Exception
+	 */
+	public void testOneAssociatedObjectNestedSearch1() throws Exception
+	{
+
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+		criteria.setId(new Integer(1));
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(1,results.length);
+		
+		MemberO2MBS result = (MemberO2MBS)results[0];
+		assertNotNull(result);
+		assertNotNull(result.getId());
+		assertNotNull(result.getName());	
+		
+		Object[] mentorCollection = getAssociationResults(result, "mentorCollection", 0);
+		assertEquals(2,mentorCollection.length);
+		
+		MemberO2MBS mentor0 = (MemberO2MBS)mentorCollection[0];
+		assertNotNull(mentor0);
+		assertNotNull(mentor0.getId());
+		assertNotNull(mentor0.getName());
+		assertEquals(true,mentor0.getId()>1 && mentor0.getId()<4);
+		
+		MemberO2MBS mentor1 = (MemberO2MBS)mentorCollection[1];
+		assertNotNull(mentor1);
+		assertNotNull(mentor1.getId());
+		assertNotNull(mentor1.getName());
+		assertEquals(true,mentor0.getId()>1 && mentor0.getId()<4);	
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search to get associated object
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * Verified the Id attribute's value of the returned object 
+	 * 
+	 * @throws Exception
+	 */
+	public void testOneAssociatedObjectNestedSearch2() throws Exception
+	{
+
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+		criteria.setId(new Integer(1));
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(1,results.length);
+		
+		MemberO2MBS result = (MemberO2MBS)results[0];
+		assertNotNull(result);
+		assertNotNull(result.getId());
+		assertEquals(new Integer(1),result.getId());		
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search to get associated object
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * Verified the Id attribute's value of the returned object 
+	 * 
+	 * @throws Exception
+	 */
+	public void testOneAssociatedObjectNestedSearch3() throws Exception
+	{
+	
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+		criteria.setId(new Integer(1));
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(1,results.length);
+		
+		MemberO2MBS result = (MemberO2MBS)results[0];
+		assertNotNull(result);
+		assertNotNull(result.getId());
+		assertEquals(new Integer(1),result.getId());		
+	}	
+
+	public void testGetAssociation() throws Exception
+	{
+		Class targetClass = MemberO2MBS.class;
+		MemberO2MBS criteria = new MemberO2MBS();
+
+		Object[] results = getQueryObjectResults(targetClass, criteria);
+
+		assertNotNull(results);
+		assertEquals(4,results.length);
+		
+		MemberO2MBS mentor = (MemberO2MBS)results[0];
+		assertNotNull(mentor);
+		assertNotNull(mentor.getId());
+		assertNotNull(mentor.getName());	
+		
+		Object[] selfResults = getAssociationResults(mentor, "self", 0);
+		assertEquals(1,selfResults.length);
+		
+		MemberO2MBS self = (MemberO2MBS)selfResults[0];
+		assertNotNull(self);
+		assertNotNull(self.getId());
+		assertNotNull(self.getName());	
+	}	
+}
---
+++ sdk-toolkit/example-project/junit/src/test/xml/data/O2OBidirectionalSelfassociationXMLDataTest.java
@@ -0,0 +1,349 @@
+package test.xml.data;
+
+
+import gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS;
+
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import test.xml.mapping.SDKXMLDataTestBase;
+
+public class O2OBidirectionalSelfassociationXMLDataTest extends SDKXMLDataTestBase
+{
+	public static String getTestCaseName()
+	{
+		return "One to One Bidirectional Self Association XML Data Test Case";
+	}
+	
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * 
+	 * @throws Exception
+	 */
+	public void testEntireObjectNestedSearch1() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(4,results.size());
+		
+		for(Iterator i = results.iterator();i.hasNext();)
+		{
+			MemberO2OBS result = (MemberO2OBS)i.next();
+			toXML(result);
+			
+			validateClassElements(result);
+			validateAttribute(result,"id",result.getId());
+			validateAttribute(result,"name",result.getName());
+			
+			assertTrue(validateXMLData(result, searchObject.getClass()));
+
+			MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+			
+			assertNotNull(result2);
+			assertNotNull(result2.getId());
+			assertNotNull(result2.getName());
+		}
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * 
+	 * @throws Exception
+	 */
+	
+	public void testEntireObjectNestedSearch2() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(4,results.size());
+		
+		for(Iterator i = results.iterator();i.hasNext();)
+		{
+			MemberO2OBS result = (MemberO2OBS)i.next();
+			toXML(result);
+			
+			validateClassElements(result);
+			validateAttribute(result,"id",result.getId());
+			validateAttribute(result,"name",result.getName());
+			
+			assertTrue(validateXMLData(result, searchObject.getClass()));
+
+			MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+			
+			assertNotNull(result2);
+			assertNotNull(result2.getId());
+			assertNotNull(result2.getName());			
+		}
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * erifies that the associated object is null
+	 * 
+	 * @throws Exception
+	 */
+	public void testZeroAssociatedObjectsNestedSearch1() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		searchObject.setId(new Integer(3));
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		Iterator i = results.iterator();
+		MemberO2OBS result = (MemberO2OBS)i.next();
+		toXML(result);
+		MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+		
+		assertNotNull(result2);
+		assertNotNull(result2.getId());
+		assertNotNull(result2.getName());
+		
+		MemberO2OBS mentor = result2.getMentor();
+		assertNull(mentor);
+	}
+	
+	/**
+	 * Uses Nested Search Criteria for search
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * Verifies that the associated object has required Id
+	 * 
+	 * @throws Exception
+	 */
+	public void testOneAssociatedObjectNestedSearch1() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		searchObject.setId(new Integer(1));
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		Iterator i = results.iterator();
+		MemberO2OBS result = (MemberO2OBS)i.next();
+		toXML(result);
+		MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+		
+		assertNotNull(result2);
+		assertNotNull(result2.getId());
+		assertNotNull(result2.getName());
+		
+		MemberO2OBS mentor = result2.getMentor();
+		assertNotNull(mentor);
+		assertNotNull(mentor.getId());
+		assertNotNull(mentor.getName());
+		assertEquals(new Integer(2),mentor.getId());
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search to get associated object
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * Verified the Id attribute's value of the returned object 
+	 * 
+	 * @throws Exception
+	 */
+	public void testOneAssociatedObjectNestedSearch2() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		searchObject.setId(new Integer(1));
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		Iterator i = results.iterator();
+		
+		MemberO2OBS mentor = (MemberO2OBS)i.next();
+		toXML(mentor);
+		MemberO2OBS result2 = (MemberO2OBS)fromXML(mentor);
+		
+		assertNotNull(result2);
+		assertNotNull(result2.getId());
+		assertNotNull(result2.getName());
+		assertEquals(new Integer(1),result2.getId());
+	}
+
+	/**
+	 * Uses Nested Search Criteria for search to get associated object
+	 * Verifies that the results are returned 
+	 * Verifies size of the result set
+	 * Verifies that none of the attribute is null
+	 * Verified the Id attribute's value of the returned object 
+	 * 
+	 * @throws Exception
+	 */
+	public void testOneAssociatedObjectNestedSearch3() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		searchObject.setId(new Integer(1));
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		Iterator i = results.iterator();
+		
+		MemberO2OBS mentor = (MemberO2OBS)i.next();
+		toXML(mentor);
+		MemberO2OBS result2 = (MemberO2OBS)fromXML(mentor);
+		
+		assertNotNull(result2);
+		assertNotNull(result2.getId());
+		assertNotNull(result2.getName());
+		assertEquals(new Integer(1),result2.getId());
+	}	
+
+	public void testGetMethods1() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		searchObject.setId(new Integer(1));
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		MemberO2OBS result = (MemberO2OBS)results.iterator().next();
+		toXML(result);
+		MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+		assertEquals(new Integer(2),result2.getMentor().getId());
+
+		searchObject.setId(new Integer(2));
+		results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		result = (MemberO2OBS)results.iterator().next();
+		toXML(result);
+		result2 = (MemberO2OBS)fromXML(result);
+		assertEquals(new Integer(1),result2.getMentor().getId());
+
+		searchObject.setId(new Integer(3));
+		results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		result = (MemberO2OBS)results.iterator().next();
+		toXML(result);
+		result2 = (MemberO2OBS)fromXML(result);
+		assertNull(result2.getMentor());
+		
+	}
+
+
+	public void testGetMethods2() throws Exception
+	{
+		MemberO2OBS searchObject = new MemberO2OBS();
+		searchObject.setId(new Integer(1));
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		MemberO2OBS result = (MemberO2OBS)results.iterator().next();
+		toXML(result);
+		MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+		assertEquals(new Integer(1),result2.getSelf().getId());
+
+		searchObject.setId(new Integer(2));
+		results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		result = (MemberO2OBS)results.iterator().next();
+		toXML(result);
+		result2 = (MemberO2OBS)fromXML(result);
+		assertEquals(new Integer(2),result2.getSelf().getId());
+
+		searchObject.setId(new Integer(3));
+		results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(1,results.size());
+		
+		result = (MemberO2OBS)results.iterator().next();
+		toXML(result);
+		result2 = (MemberO2OBS)fromXML(result);
+		assertNotNull(result2.getSelf());
+		
+	}
+	
+	public void testGetAssociation1() throws Exception
+	{
+
+		MemberO2OBS searchObject = new MemberO2OBS();
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(4,results.size());
+		
+		MemberO2OBS mentor;
+		for(Iterator i = results.iterator();i.hasNext();)
+		{
+			MemberO2OBS result = (MemberO2OBS)i.next();
+			toXML(result);
+			MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+			assertNotNull(result2);
+			assertNotNull(result2.getId());
+			assertNotNull(result2.getName());
+			
+			if (result2.getId() < 3){ // MemberO2OBS id = 3 does not have an associated Mentor	
+				validateAssociation(result,"MemberO2OBS","mentor");
+				mentor = result2.getMentor();
+				assertNotNull(mentor);
+				assertNotNull(mentor.getId());
+				assertNotNull(mentor.getName());
+			}
+		}
+	}
+	
+	public void testGetAssociation2() throws Exception
+	{
+
+		MemberO2OBS searchObject = new MemberO2OBS();
+		Collection results = getApplicationService().search("gov.nih.nci.cacoresdk.domain.onetoone.bidirectional.selfassociation.MemberO2OBS",searchObject );
+
+		assertNotNull(results);
+		assertEquals(4,results.size());
+		
+		MemberO2OBS mentor;
+		for(Iterator i = results.iterator();i.hasNext();)
+		{
+			MemberO2OBS result = (MemberO2OBS)i.next();
+			toXML(result);
+			MemberO2OBS result2 = (MemberO2OBS)fromXML(result);
+			assertNotNull(result2);
+			assertNotNull(result2.getId());
+			assertNotNull(result2.getName());
+			
+			if (result2.getId() < 3){ // MemberO2OBS id = 3,4, and 5 don't have an associated MemberO2OBS
+				validateAssociation(result,"MemberO2OBS","mentor");
+				mentor = result2.getMentor();
+				assertNotNull(mentor);
+				assertNotNull(mentor.getId());
+				assertNotNull(mentor.getName());
+			}
+		}
+	}
+		
+}
