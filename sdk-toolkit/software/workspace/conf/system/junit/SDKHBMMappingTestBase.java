a861025b61c0362643100cd44ee86e882a13d6b0
Prasad Konka <prasad.konka@nih.gov>
2012-11-26 22:04:30 +0000

    Fix for SDK-1469
    git-svn-id: https://ncisvn.nci.nih.gov/svn/cacoresdk/branches/CACORESDK_45_BRANCH@1923 221b6c9c-8bdd-4aed-8ce9-b1cbd18b7e1b

---
+++ sdk-toolkit/software/workspace/conf/system/junit/SDKHBMMappingTestBase.java
@@ -0,0 +1,70 @@
+package test;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.jaxen.JaxenException;
+import org.jaxen.jdom.JDOMXPath;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.springframework.core.io.ClassPathResource;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import junit.framework.TestCase;
+
+public abstract class SDKHBMMappingTestBase extends TestCase {
+
+	private String uriPrefix = "gme://caCORE.caCORE/3.2/";
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	protected static String getTestCaseName() {
+		return "SDK HBM Mapping Test Base";
+	}
+
+	public abstract Document getDoc();
+
+	protected Document getDocument(String filename) {
+		Document doc;
+		try {
+			SAXBuilder builder = new SAXBuilder();
+			ClassPathResource classPathResource=new ClassPathResource(filename);
+			builder.setEntityResolver(new HibernateDTDResolver());
+			doc = builder.build(classPathResource.getInputStream());
+		} catch (Exception ex) {
+			throw new RuntimeException("Error reading XML Mapping file: " + filename,ex);
+		}
+		return doc;
+	}
+
+	protected List<Element> queryXMLMapping(Object obj, String xpath)
+			throws JaxenException {
+
+		// e.g.: xpath = "/*[local-name()='schema']";
+		JDOMXPath path = new JDOMXPath(xpath);
+		List<Element> elts = path.selectNodes(obj);
+		return elts;
+	}
+
+	private class HibernateDTDResolver implements EntityResolver {
+		public InputSource resolveEntity(String publicID, String systemID)
+				throws SAXException, IOException {
+			if (systemID.equals("http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd")) {
+				// Return local copy of the hibernate-mapping-3.0.dtd file
+				ClassPathResource resource=new ClassPathResource("org/hibernate/hibernate-mapping-3.0.dtd");
+				return new InputSource(resource.getInputStream());
+			}
+			// If no match, returning null makes process continue normally
+			return null;
+		}
+	}
+}
